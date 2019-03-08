2729fd1a9c9c6b828db914b49dc5ea2709a33902
Max Neuvians <max@neuvians.net>
2019-03-08 11:07:16 -0500

    Wildcard for dockerfiles

--- docker-lint/entrypoint.sh
+++ docker-lint/entrypoint.sh
@@ -5,7 +5,7 @@ echo ""
 echo "Searching for Dockerfiles ..."
 echo "-----------------------------"
 
-for f in $(find $GITHUB_WORKSPACE -name "Dockerfile"); do
+for f in $(find $GITHUB_WORKSPACE -name "Dockerfile*"); do
     echo "Processing $f"
     while IFS= read -r line; do
       data+=( "$line" )
